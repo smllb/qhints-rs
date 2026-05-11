63f85c6cb178c03449d687742adaae24b3d89c4d
smllb <smllb@users.noreply.github.com>
2026-05-11 22:28:57 +0000

    deploy: 2522a73e32d5053260b313dc6f88f27cf41d9235

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bc2af10e.js',
+                path_to_root + 'searchindex-bbfbb6c0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
